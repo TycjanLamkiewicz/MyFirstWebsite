3e02faab74c6a8674af1b981078f89cfb8aa238e
TycjanLamkiewicz <altairkot@gmail.com>
2020-10-02 23:34:48 +0200

    add randomNumber function and fix bugs

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ function countingDown()
 var today = new Date();
 
 var day = today.getDate();
+if(day < 10) day = "0" + day;
+
 var month = today.getMonth();
+if(month < 10) month = "0" + month;
+
 var year = today.getFullYear();
 
 var hour = today.getHours();
@@ -43,3 +47,12 @@ function printCurrentPage()
 {
 window.print();
 }
+
+
+
+
+
+
+
+
+console.log(Math.random());
